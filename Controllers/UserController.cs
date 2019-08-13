e0a78acecaacc4893f0c85e6255a79bb2b4656df
Deepak Shrivastav <dshrivastav@affinionds.com>
2019-08-13 17:59:33 +0530

    Basic Api Project Completed

---
+++ Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public Dictionary<string, string> msgs = new Dictionary<string, string>
+        {
+            {"hi", "hello" },
+            {"hello", "hi" }
+        };
+        [HttpGet("{msg}")]
+        public ActionResult<string> Get(string msg)
+        {
+            return msgs.ContainsKey(msg) ? msgs[msg] : "invalid";
+        }
+
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        Dictionary<string, string> users = new Dictionary<string, string>
+        {
+            {"1", "deepak" },
+            {"2", "sandesh" },
+            {"3", "ankit" },
+            {"4", "gjh" },
+            {"5", "deesidlucliepak" },
+            {"6", "dexdsxszsdcepak" },
+            {"7", "dedxdxepak" }
+        };
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            string[] temp =  new string[users.Count];
+            int t = 0;
+            foreach (var i in users)
+                temp[t++] = i.Value;
+            return temp;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return users[id.ToString()];
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            Console.WriteLine(value);
+            users.Add((users.Count + 1).ToString(), value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            users[id.ToString()] = value;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            users.Remove(id.ToString());
+        }
+    }
+}
